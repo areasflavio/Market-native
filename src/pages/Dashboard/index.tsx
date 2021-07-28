442bfaba1ac92d76ea2ea242d34c381bab9fcf95
Flávio Arêas <vflavio9@gmail.com>
2021-07-28 20:00:45 -0300

    feat: dashboard screen ui

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import CartBar from '../../components/CartBar';
 
-import { Container } from './styles';
+import formatPrice from '../../utils/formatPrice';
+
+import {
+	Container,
+	ProductContainer,
+	ProductCard,
+	ProductImage,
+	ProductPrice,
+	ProductPriceText,
+	ProductPriceButton,
+	ProductTitle,
+	ProductsList,
+} from './styles';
+
+interface Product {
+	id: string;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+}
 
 const Dashboard: React.FC = () => {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		async function loadProducts(): Promise<void> {
+			const response = await axios.get('https://fakestoreapi.com/products');
+
+			setProducts(response.data);
+		}
+
+		loadProducts();
+	}, []);
+
 	return (
 		<Container>
+			<ProductContainer>
+				<ProductsList
+					data={products}
+					keyExtractor={item => item.id}
+					ListFooterComponent={<View />}
+					ListFooterComponentStyle={{
+						height: 80,
+					}}
+					renderItem={({ item }) => (
+						<ProductCard>
+							<ProductImage
+								source={{
+									uri: item.image,
+								}}
+							/>
+							<ProductTitle>{item.title}</ProductTitle>
+
+							<ProductPrice>
+								<ProductPriceText>{formatPrice(item.price)}</ProductPriceText>
+								<ProductPriceButton>
+									<FeatherIcon name="plus" size={20} color="#8F8F8F" />
+								</ProductPriceButton>
+							</ProductPrice>
+						</ProductCard>
+					)}
+				/>
+			</ProductContainer>
 			<CartBar />
 		</Container>
 	);
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,72 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View`
+interface Product {
+	id: string;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+}
+
+export const Container = styled.SafeAreaView`
 	flex: 1;
+	align-items: center;
 `;
+
+export const ProductContainer = styled.View`
+	flex: 1;
+	flex-direction: row;
+
+	margin-top: 60px;
+	border-radius: 5px;
+`;
+
+export const ProductsList = styled(
+	FlatList as new () => FlatList<Product>
+).attrs({
+	numColumns: 2,
+})`
+	flex: 1;
+	padding: 0 10px;
+`;
+export const ProductCard = styled.View`
+	flex: 1;
+
+	padding: 16px 16px;
+	margin: 8px;
+	border-radius: 4px;
+
+	background: #fff;
+`;
+
+export const ProductImage = styled.Image`
+	align-self: center;
+	height: 122px;
+	width: 122px;
+`;
+
+export const ProductTitle = styled.Text`
+	font-size: 14px;
+	margin-top: 10px;
+`;
+
+export const ProductPrice = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+
+	padding-top: 10px;
+	margin-top: auto;
+`;
+
+export const ProductPriceText = styled.Text`
+	font-weight: bold;
+	font-size: 16px;
+	color: #1e90ff;
+`;
+
+export const ProductPriceButton = styled.TouchableOpacity.attrs({
+	activeOpacity: 0.6,
+})``;
